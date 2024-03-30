acc3c5fc132a9362fe86e9875cc354a017849b38
Maria Monpeat <115364869+mmonpeat@users.noreply.github.com>
2024-03-30 10:49:18 +0100

    Update ft_memchr.c

--- libft/ft_memchr.c
+++ libft/ft_memchr.c
@@ -20,7 +20,7 @@ void	*ft_memchr(const void *s, int c, size_t n)
 	while (i < n)
 	{
 		if (((unsigned char *)s)[i] == (unsigned char)c)
-			return ((void *)&(((unsigned char *)s)[i]);
+			return ((void *)&(((unsigned char *)s)[i]));
 		i++;
 	}
 	return (NULL);
